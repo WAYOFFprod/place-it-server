a061b4974073318a1a0d828ec235dafb86d0c8c7
WAYOFFprod <raphael@wayoff.ch>
2024-06-21 13:13:13 +0200

    added authentication for client

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,11 @@ use App\Http\Controllers\CanvaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function (Request $request) {
-    return "yep";
+/**
+ * Canvas
+ */
+//liveserver
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/place-pixel', [CanvaController::class, 'placePixel'])
+        ->middleware(['abilities:canvas:place-pixels']);
 });
-
-// Auth
-Route::post('tokens/create', [AuthController::class, 'create']);
-
-
-// Canvas
-Route::get('/canva/{id}', [CanvaController::class, 'getCanva']);
-Route::post('/place-pixel', [CanvaController::class, 'placePixel'])
-    ->middleware(['auth:sanctum', 'abilities:canvas:place-pixels']);;
-Route::post('/canvas/create', [CanvaController::class, 'createCanva']);
-Route::post('/canvas/color/add', [CanvaController::class, 'addColors']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CanvaController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+/**
+ * Canvas
+ */
+
+ // authenticated
+ Route::middleware('auth')->group(function () {
+     Route::post('/canvas/color/add', [CanvaController::class, 'addColors']);
+     Route::post('/canvas/create', [CanvaController::class, 'createCanva']);
+ });
+
+ // public
+Route::get('/canvas/{id}', [CanvaController::class, 'getCanva']);
